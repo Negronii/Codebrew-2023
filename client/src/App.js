cd38d89ef45a838dc0d0bf74aa9224121c284ff6
Yixiang <yixiangw3@student.unimelb.edu.au>
2023-04-15 13:53:09 +1000

    Add login and register route

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SessionPage from 'pages/sessionPage';
 import VolunteerPage from 'pages/volunteerPage';
 import ChatPage from 'pages/chatPage';
+import LoginPage from 'pages/login';
+import RegisterPage from 'pages/register';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+        <Route path="/register" element={<RegisterPage />} />
+          <Route path="/" element={<LoginPage />} />
           <Route path="/session" element={<SessionPage />} />
           <Route path="/volunteer" element={<VolunteerPage />} />
           <Route path="/chat" element={<ChatPage />} />
